99bacbedd64b2c38b6b891aa967382142e2306ec
Daniela <83104187+Dani141@users.noreply.github.com>
2022-08-11 16:35:22 -0400

    Social Media

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
-builder.Services.AddSingleton<IEmployersService, EmployersService>();
+builder.Services.AddSingleton<IStartersService, StartersService>();
 
 var app = builder.Build();
 
